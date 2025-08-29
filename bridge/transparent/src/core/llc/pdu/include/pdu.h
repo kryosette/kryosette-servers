52fc2e432c4d7f63746449f4b78b2b7253bb4650
dima853 <kosiktv546@gmail.com>
2025-08-29 13:26:06 +0300

    something create

--- bridge/transparent/src/core/llc/pdu/include/pdu.h
+++ bridge/transparent/src/core/llc/pdu/include/pdu.h
@@ -1,17 +1,70 @@
-#include "llc.h"
-#include "forwarding.h"
+#pragma once
+#ifndef PDU_H
+#define PDU_H
 
-extern int llc_sap_get_handler(lsap_t sap, llc_sap_handler_fn *handler);
+#include <cstdint>
+#include <cstddef>
 
-void llc_receive_frame(port_id_t port, const uint8_t *frame_data, size_t frame_len)
+struct dsap_address
 {
-    llc_parsed_pdu_t pdu;
-    int parse_result = llc_pdu_parse(frame_data, frame_len, &pdu);
+    bool is_group : 1;
+    uint8_t address : 7;
+};
 
-    if (parse_result != 0)
+struct ssap_address
+{
+    bool is_response : 1;
+    uint8_t address : 7;
+};
+
+struct supervisory_type
+{
+    uint8_t pr : 2;
+    uint8_t rej : 2;
+    uint8_t rnr : 2;
+};
+
+union control_field
+{
+    uint16_t full_word;
+
+    struct
     {
-        return;
-    }
+        uint8_t receive_sequence : 7;
+        bool poll_final : 1;
+        uint8_t send_sequence : 7;
+        uint8_t format : 1;
+    } information;
 
-    llc_sap_handler_fn handler = NULL;
-}
+    struct
+    {
+        uint8_t modifier_bits : 2;
+        uint8_t format : 2;
+        uint8_t command_type : 4;
+    } unnumbered;
+
+    struct
+    {
+        uint8_t receive_sequence : 7;
+        bool poll_final : 1;
+        uint8_t reserved : 2;
+        supervisory_type type;
+        uint8_t format : 2;
+    } supervisory;
+};
+
+struct info_field
+{
+    uint8_t *data;
+    size_t length;
+};
+
+struct llc_pdu
+{
+    dsap_address dsap;
+    ssap_address ssap;
+    control_field control;
+    info_field information;
+};
+
+#endif
