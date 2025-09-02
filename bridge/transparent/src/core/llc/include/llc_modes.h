a3616c52611344fbc61768d70f08b1500acebe25
dima853 <kosiktv546@gmail.com>
2025-09-02 09:27:44 +0300

    Update llc_modes.h

--- bridge/transparent/src/core/llc/include/llc_modes.h
+++ bridge/transparent/src/core/llc/include/llc_modes.h
@@ -1,1 +1,17 @@
+#pragma once
+#ifndef LLC_MODES_H
+#define LLC_MODES_H
 
+#include <stdint>
+
+typedef struct llc_connection llc_connection_t; 
+
+typedef struct {
+  llc_connection_t *active_connection;
+  uint8_t dm_response_count;
+  int is_listening_for_setup;
+} llc_abm_data_t;
+
+
+
+#endif
