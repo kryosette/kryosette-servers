46b4d50cfe00853f0da7ae5d7898c7291660a3e8
dima853 <kosiktv546@gmail.com>
2025-09-02 09:35:14 +0300

    Update llc_modes.h

--- bridge/transparent/src/core/llc/include/llc_modes.h
+++ bridge/transparent/src/core/llc/include/llc_modes.h
@@ -12,6 +12,16 @@ typedef struct {
   int is_listening_for_setup;
 } llc_abm_data_t;
 
+typedef struct {
+  enum {
+.   LLC_GLOBAL_MODE_ABM
+  } current_mode;
 
+  union {
+    llc_abm_data_t abm_data;
+  }
+} llc_station_global_state_t;
+
+void llc_switch_to_abm_mode(llc_connection *conn);
 
 #endif
