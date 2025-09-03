72ec8105e74e493344775dee292b0f327617f74f
dima853 <kosiktv546@gmail.com>
2025-09-03 10:05:29 +0300

    Update llc_core.c

--- bridge/transparent/src/core/llc/llc_core.c
+++ bridge/transparent/src/core/llc/llc_core.c
@@ -2,3 +2,8 @@
 #include "forwarding.h" 
 #include "filtering.h" 
 #include "stp.h"      
+#include "llc_core.h"
+
+void llc_receive_ip(uint8_t *ip_packet, size_t len) {
+  if (ip_packet == NULL || len == 0) return 0;
+}
