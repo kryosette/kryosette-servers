e8188269a3964c63fed5ceb3bf262d75de357ddd
dima853 <kosiktv546@gmail.com>
2025-09-03 10:12:22 +0300

    Update llc_core.c

--- bridge/transparent/src/core/llc/llc_core.c
+++ bridge/transparent/src/core/llc/llc_core.c
@@ -2,8 +2,15 @@
 #include "forwarding.h" 
 #include "filtering.h" 
 #include "stp.h"      
-#include "llc_core.h"
 
 void llc_receive_ip(uint8_t *ip_packet, size_t len) {
   if (ip_packet == NULL || len == 0) return 0;
+
+  uint8_t *llc_pdu = llc_encapsulate_ip(ip_packet, len);
+
+  if (llc_pdu != NULL) {
+    mac_send(llc_pdu, LLC_FRAME_SIZE(len);
+
+    free(llc_pdu);
+  }
 }
