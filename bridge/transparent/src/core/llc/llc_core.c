3ecdbb083c09facc3e52e39b15708362df857cf5
dima853 <kosiktv546@gmail.com>
2025-09-03 10:53:36 +0300

    Update llc_core.c

--- bridge/transparent/src/core/llc/llc_core.c
+++ bridge/transparent/src/core/llc/llc_core.c
@@ -16,22 +16,28 @@ void llc_receive_ip(uint8_t *ip_packet, size_t len) {
 }
 
 // ip header to llc/snap
-uint8_t llc_encapsulate_ip(const uint8_t ip_packet, size_t len) {
+uint8_t llc_encapsulate_ip(const uint8_t ip_packet, size_t ip_len) {
   size_t llc_frame_size = sizeof(llc_header_t) + sizeof(snap_header_t) + ip_len;
  
   uint8_t *llc_frame = (uint8_t*) malloc(llc_frame_size);
   if (llc_frame == NULL) return NULL;
+
+  uint8_t *ptr = llc_frame;
   
   llc_header_t *llc_hdr = (llc_header_t*)llc_frame;
   llc_hdr->dsap = DSAP_SNAP;
   llc_hdr->ssap = SSAP_SNAP;
-  llc_hdr->control = 0x01; // type 2
-
+  llc_hdr->control = CTRL_UNNUMBERED; // type 2
+  ptr += sizeof(llc_header_t);
+  
   snap_header_t *snap_hdr = (snap_header_t*)snap_frame;
   snap_hdr->oui[0] = 0x00;
   shap_hdr->oui[1] = 0x00;
   snap_hdr->oui[2] = 0x00;
-  snap_hdr->pid = htons(0x0800);
+  snap_hdr->pid = htons(ETH_P_IP); 
+  ptr += sizeof(snap_header_t);
 
-  
+  memcpy(ptr, ip_packet, ip_len);
+
+  return llc_frame;
 }
