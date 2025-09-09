55b91f2ba4cb3b7c9aa75d284f6049c7729b41c7
dima853 <kosiktv546@gmail.com>
2025-09-09 10:47:07 +0300

    Update stp.c

--- bridge/transparent/src/core/stp/stp.c
+++ bridge/transparent/src/core/stp/stp.c
@@ -3,6 +3,18 @@
 struct stp_instance *stp;
 struct mac_table_t *mac;
 
+int bridge_id_compare(const struct bridge_id *a, const struct bridge_id *b) {
+    if (a->priority != b->priority) {
+        return a->priority - b->priority;
+    }
+    return memcmp(a->mac, b->mac, 6);
+}
+
+void init_bridge_id(struct bridge_id *bid, uint16_t priority, const uint8_t *mac) {
+    bid->priority = priority;
+    memcpy(bid->mac, mac, 6);
+}
+
 void stp_init(struct stp_instance *stp, uint8_t *mac) {
   if (stp == NULL) {
     return NULL;
