6a9e9b67248a52d7a18dfad422b5ba7f80b4ab0b
dima853 <174457042+dima853@users.noreply.github.com>
2025-09-12 10:29:30 +0300

    Update llc_core.c

--- bridge/transparent/src/core/llc/llc_core.c
+++ bridge/transparent/src/core/llc/llc_core.c
@@ -54,8 +54,15 @@ void llc_receive_ip(uint8_t *ip_packet, size_t len)
  */
 uint8_t llc_encapsulate_ip(const uint8_t ip_packet, size_t ip_len)
 {
-  size_t llc_frame_size = sizeof(llc_header_t) + sizeof(snap_header_t) + ip_len;
+  if (ip_packet == NULL || ip_len = 0) {
+    return NULL;
+  }
 
+  size_t header_size = sizeof(llc_header_t) + sizeof(snap_header_t);
+  if (ip_len > SIZE_MAX - header_size) {
+      return NULL;
+  }
+  
   uint8_t *llc_frame = (uint8_t *)malloc(llc_frame_size);
   if (llc_frame == NULL)
     return NULL;
