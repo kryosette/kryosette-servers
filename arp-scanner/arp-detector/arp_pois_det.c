0fd63515fce046fe245fcf684b9a4b1280e80ce5
dima853 <174457042+dima853@users.noreply.github.com>
2025-09-21 21:04:49 +0300

    something create

--- arp-scanner/arp-detector/arp_pois_det.c
+++ arp-scanner/arp-detector/arp_pois_det.c
@@ -10,47 +10,46 @@
 #include <sys/ioctl.h>
 #include <signal.h>
 
-// Явно объявляем структуру ARP-заголовка, так как она может быть не определена в стандартных заголовках
-struct arphdr {
-    unsigned short int ar_hrd;      /* Format of hardware address.  */
-    unsigned short int ar_pro;      /* Format of protocol address.  */
-    unsigned char ar_hln;           /* Length of hardware address.  */
-    unsigned char ar_pln;           /* Length of protocol address.  */
-    unsigned short int ar_op;       /* ARP opcode (command).  */
+struct arphdr
+{
+    unsigned short int ar_hrd; /* Format of hardware address.  */
+    unsigned short int ar_pro; /* Format of protocol address.  */
+    unsigned char ar_hln;      /* Length of hardware address.  */
+    unsigned char ar_pln;      /* Length of protocol address.  */
+    unsigned short int ar_op;  /* ARP opcode (command).  */
     /* Hardware and protocol addresses. */
-    unsigned char __ar_sha[6];      /* Sender hardware address.  */
-    unsigned char __ar_sip[4];      /* Sender IP address.  */
-    unsigned char __ar_tha[6];      /* Target hardware address.  */
-    unsigned char __ar_tip[4];      /* Target IP address.  */
+    unsigned char __ar_sha[6]; /* Sender hardware address.  */
+    unsigned char __ar_sip[4]; /* Sender IP address.  */
+    unsigned char __ar_tha[6]; /* Target hardware address.  */
+    unsigned char __ar_tip[4]; /* Target IP address.  */
 };
 
-#define ARPOP_REPLY 2 // Код ответа ARP
+#define ARPOP_REPLY 2
 
-// Структура для доверенной записи
-typedef struct {
-    uint32_t ip;        // IP в network byte order
-    unsigned char mac[6]; // MAC-адрес
+typedef struct
+{
+    uint32_t ip;
+    unsigned char mac[6];
 } trusted_pair_t;
 
-// Белый список (доверенные пары IP-MAC). Инициализируем в main.
 trusted_pair_t *trusted_list;
 size_t num_trusted = 0;
 
-// Функция для проверки, является ли ARP-пакет подозрительным
-int is_arp_poisoning(const struct arphdr *arp_hdr, const unsigned char *src_mac) {
-    // Проверяем, это ARP-ответ?
-    if (ntohs(arp_hdr->ar_op) != ARPOP_REPLY) {
-        return 0; // Это не ответ, пропускаем
+int is_arp_poisoning(const struct arphdr *arp_hdr, const unsigned char *src_mac)
+{
+    if (ntohs(arp_hdr->ar_op) != ARPOP_REPLY)
+    {
+        return 0;
     }
 
-    // Проходим по белому списку
-    for (size_t i = 0; i < num_trusted; i++) {
-        // Если IP из пакета есть в белом списке
-        if (memcmp(arp_hdr->__ar_sip, &trusted_list[i].ip, sizeof(uint32_t)) == 0) {
-            // Но MAC-адрес отправителя НЕ совпадает с доверенным
-            if (memcmp(src_mac, trusted_list[i].mac, 6) != 0) {
+    for (size_t i = 0; i < num_trusted; i++)
+    {
+        if (memcmp(arp_hdr->__ar_sip, &trusted_list[i].ip, sizeof(uint32_t)) == 0)
+        {
+            if (memcmp(src_mac, trusted_list[i].mac, 6) != 0)
+            {
                 printf("[!] ALERT: ARP Poisoning detected!\n");
-                printf("    For IP: %s\n", inet_ntoa(*(struct in_addr*)&trusted_list[i].ip));
+                printf("    For IP: %s\n", inet_ntoa(*(struct in_addr *)&trusted_list[i].ip));
                 printf("    Expected MAC: %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n",
                        trusted_list[i].mac[0], trusted_list[i].mac[1],
                        trusted_list[i].mac[2], trusted_list[i].mac[3],
@@ -58,21 +57,21 @@ int is_arp_poisoning(const struct arphdr *arp_hdr, const unsigned char *src_mac)
                 printf("    Received MAC: %.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n",
                        src_mac[0], src_mac[1], src_mac[2],
                        src_mac[3], src_mac[4], src_mac[5]);
-                return 1; // Обнаружена атака!
+                return 1;
             }
         }
     }
-    return 0; // Всё ок
+    return 0;
 }
 
-int main() {
-    // Инициализируем белый список вручную (можно вынести в конфиг)
+int main()
+{
     num_trusted = 2;
     trusted_list = malloc(num_trusted * sizeof(trusted_pair_t));
-    
+
     trusted_list[0].ip = inet_addr("192.168.1.1");
     memcpy(trusted_list[0].mac, (unsigned char[]){0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}, 6);
-    
+
     trusted_list[1].ip = inet_addr("192.168.1.100");
     memcpy(trusted_list[1].mac, (unsigned char[]){0x08, 0x00, 0x27, 0x96, 0x20, 0x39}, 6);
 
@@ -83,7 +82,8 @@ int main() {
 
     // Создаём RAW-сокет для приёма всех ARP-пакетов
     sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
-    if (sockfd < 0) {
+    if (sockfd < 0)
+    {
         perror("socket");
         free(trusted_list);
         exit(EXIT_FAILURE);
@@ -91,10 +91,12 @@ int main() {
 
     printf("[+] ARP Guardian started. Monitoring for ARP spoofing...\n");
 
-    while (1) {
+    while (1)
+    {
         // Читаем приходящий пакет
-        int recv_len = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&saddr, &saddr_len);
-        if (recv_len < 0) {
+        int recv_len = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&saddr, &saddr_len);
+        if (recv_len < 0)
+        {
             perror("recvfrom");
             close(sockfd);
             free(trusted_list);
@@ -102,17 +104,19 @@ int main() {
         }
 
         // Проверяем, что пакет достаточно большой для Ethernet + ARP
-        if (recv_len < (int)(sizeof(struct ethhdr) + sizeof(struct arphdr))) {
+        if (recv_len < (int)(sizeof(struct ethhdr) + sizeof(struct arphdr)))
+        {
             continue;
         }
 
         // Парсим Ethernet-заголовок
-        struct ethhdr *eth_hdr = (struct ethhdr*)buffer;
+        struct ethhdr *eth_hdr = (struct ethhdr *)buffer;
         // Парсим ARP-заголовок (после Ethernet-заголовка)
-        struct arphdr *arp_hdr = (struct arphdr*)(buffer + sizeof(struct ethhdr));
+        struct arphdr *arp_hdr = (struct arphdr *)(buffer + sizeof(struct ethhdr));
 
         // Проверяем пакет на атаку
-        if (is_arp_poisoning(arp_hdr, eth_hdr->h_source)) {
+        if (is_arp_poisoning(arp_hdr, eth_hdr->h_source))
+        {
             printf("[!] Taking action...\n");
             // Здесь можно добавить реакцию
         }
---
+++ bridge/transparent/src/core/mac/include/mac.h
@@ -0,0 +1,92 @@
+#pragma once
+#ifndef MAC_H
+#define MAC_H
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
+
+#define MAC_ADDR_LEN 6         // Длина MAC-адреса
+#define ETH_MTU 1500           // Maximum Transmission Unit
+#define ETH_MIN_FRAME_LEN 64   // Минимальный размер кадра (с преамбулой)
+#define ETH_MAX_FRAME_LEN 1518 // Максимальный размер кадра (с преамбулой)
+
+#define ETH_P_LLC_SNAP 0x8870 // LLC/SNAP инкапсуляция
+#define ETH_P_IP 0x0800       // IPv4
+#define ETH_P_ARP 0x0806      // ARP
+#define ETH_P_IPV6 0x86DD     // IPv6
+
+#define ETH_MIN_FRAME_LEN 64 // Минимальная длина кадра от Преамбулы до FCS включительно (по стандарту)
+#define ETH_MIN_PAYLOAD_LEN 46
+#define ETH_FCS_LEN 4
+
+#define ETH_MIN_RX_FRAME_LEN (sizeof(eth_header_t) + ETH_MIN_PAYLOAD_LEN + ETH_FCS_LEN)
+
+extern const uint8_t MAC_BROADCAST_ADDR[MAC_ADDR_LEN]; // FF:FF:FF:FF:FF:FF
+extern const uint8_t MAC_NULL_ADDR[MAC_ADDR_LEN];      // 00:00:00:00:00:00
+
+#pragma pack(push, 1)
+
+typedef struct
+{
+    uint8_t dst_addr[MAC_ADDR_LEN];
+    uint8_t src_addr[MAC_ADDR_LEN];
+    uint16_t ethertype;
+} eth_header_t;
+
+typedef struct
+{
+    eth_header_t header;
+    uint8_t payload[ETH_MTU];
+    uint32_t fcs;
+} eth_frame_t;
+
+#pragma pack(pop)
+
+typedef enum
+{
+    MAC_STATE_IDLE,
+    MAC_STATE_SENDING,
+    MAC_STATE_RECEIVING,
+    MAC_STATE_ERROR
+} mac_state_t;
+
+typedef struct
+{
+    uint32_t tx_frames;
+    uint32_t rx_frames;
+    uint32_t tx_bytes;
+    uint32_t rx_bytes;
+    uint32_t crc_errors;
+    uint32_t collisions;
+    uint64_t rx_errors;
+    uint64_t tx_errors;
+} mac_stats_t;
+
+typedef void (*mac_rx_callback_t)(const uint8_t *data, size_t len, const uint8_t *src_addr);
+typedef void (*mac_tx_complete_callback_t)(bool success);
+
+void mac_init(const uint8_t *my_mac_addr);
+
+void mac_set_rx_callback(mac_rx_callback_t callback);
+void mac_set_tx_complete_callback(mac_tx_complete_callback_t callback);
+
+bool mac_send_frame(const uint8_t *dst_addr,
+                    const uint8_t *data,
+                    size_t data_len,
+                    uint16_t ethertype);
+
+bool mac_send_llc_pdu(const uint8_t *dst_addr, const uint8_t *llc_pdu, size_t pdu_len);
+
+void mac_receive_frame(const uint8_t *frame_data, size_t frame_len);
+
+void mac_get_address(uint8_t *buffer);
+const mac_stats_t *mac_get_stats();
+void mac_update_fcs(eth_frame_t *frame);
+
+bool mac_addr_equal(const uint8_t *addr1, const uint8_t *addr2);
+bool mac_addr_is_broadcast(const uint8_t *addr);
+bool mac_addr_is_multicast(const uint8_t *addr);
+void mac_addr_copy(uint8_t *dst, const uint8_t *src);
+
+#endif // MAC_H
