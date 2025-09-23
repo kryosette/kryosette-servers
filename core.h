5381eea9d1351d69b90c7bc579d81f25538a08cb
dima853 <dima853@kali.beeline>
2025-09-23 13:15:23 -0400

    as

---
+++ core.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef CORE_H
+#define CORE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <net/if.h>
+
+#endif
