f40a3ed3423aa13e4a8e5687056b54843ee450aa
dima853 <174457042+dima853@users.noreply.github.com>
2025-10-21 11:09:52 +0300

    Update pdu_snmp.c

--- bridge/transparent/level7/mib/snmp_v3/core/snmp_pdu/pdu_snmp.c
+++ bridge/transparent/level7/mib/snmp_v3/core/snmp_pdu/pdu_snmp.c
@@ -33,7 +33,122 @@ snmp_pdu_t *snmp_pdu_create(snmp_pdu_type_t pdu_type) {
     pdu->transport_data_len = 0;
     
     return pdu;
-  }
+}
 
+static void snmp_free_variable(struct variable_list *var) {
+    if (!var) return;
+    
+    if (var->name) {
+        free(var->name);
+    }
+    
+    switch (var->type) {
+        case ASN_OCTET_STR:
+        case ASN_BIT_STR:
+        case ASN_OPAQUE:
+            if (var->val.string) {
+                free(var->val.string);
+            }
+            break;
+            
+        case ASN_OBJECT_ID:
+            if (var->val.objid) {
+                free(var->val.objid);
+            }
+            break;
+            
+        case ASN_IPADDRESS:
+            if (var->val.string && var->val_len == 4) {
+                free(var->val.string);
+            }
+            break;
+            
+        case ASN_INTEGER:
+        case ASN_COUNTER:
+        case ASN_GAUGE:
+        case ASN_TIMETICKS:
+        case ASN_COUNTER64:
+        case ASN_NULL:
+        case ASN_BOOLEAN:
+            break;
+            
+        default:
+            if (var->val.string) {
+                free(var->val.string);
+            }
+            break;
+    }
+    
+    snmp_free_variable(var->next_variable);
+    
+    free(var);
+}
+
+void snmp_pdu_free(snmp_pdu_t *pdu) {
+    if (!pdu) return;
+    
+    if (pdu->contextEngineID) {
+        free(pdu->contextEngineID);
+        pdu->contextEngineID = NULL;
+        pdu->contextEngineIDLen = 0;
+    }
+    
+    if (pdu->contextName) {
+        free(pdu->contextName);
+        pdu->contextName = NULL;
+    }
+    
+    if (pdu->transport_data) {
+        free(pdu->transport_data);
+        pdu->transport_data = NULL;
+        pdu->transport_data_len = 0;
+    }
+    
+    if (pdu->variables) {
+        snmp_free_variable(pdu->variables);
+        pdu->variables = NULL;
+    }
+    
+    free(pdu);
+}
+
+void snmp_pdu_reset(snmp_pdu_t *pdu) {
+    if (!pdu) return;
+    
+    if (pdu->contextEngineID) {
+        free(pdu->contextEngineID);
+        pdu->contextEngineID = NULL;
+        pdu->contextEngineIDLen = 0;
+    }
+    
+    if (pdu->contextName) {
+        free(pdu->contextName);
+        pdu->contextName = NULL;
+    }
+    
+    if (pdu->transport_data) {
+        free(pdu->transport_data);
+        pdu->transport_data = NULL;
+        pdu->transport_data_len = 0;
+    }
+    
+    if (pdu->variables) {
+        snmp_free_variable(pdu->variables);
+        pdu->variables = NULL;
+    }
+    
+    pdu->command = 0;
+    pdu->reqid = 0;
+    pdu->errstat = 0;
+    pdu->errindex = 0;
+    pdu->msgID = 0;
+    pdu->msgMaxSize = 0;
+    pdu->msgFlags = 0;
+    pdu->securityModel = 0;
+    pdu->timestamp.tv_sec = 0;
+    pdu->timestamp.tv_usec = 0;
+    pdu->engineBoots = 0;
+    pdu->engineTime = 0;
+}
 
 
