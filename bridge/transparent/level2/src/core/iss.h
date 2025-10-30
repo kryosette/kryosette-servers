8c85782e29be7b75b42f43debfa2ff59eb4935d5
dima853 <174457042+dima853@users.noreply.github.com>
2025-10-30 17:21:00 +0300

    something create

--- bridge/transparent/level2/src/core/iss.h
+++ bridge/transparent/level2/src/core/iss.h
@@ -6,14 +6,16 @@
 #include <linux/skbuff.h>
 #include <linux/if_ether.h>
 
-typedef enum {
+typedef enum
+{
     MA_DATA_REQUEST,
     MA_DATA_INDICATION,
     MA_CONTROL_REQUEST,
     MA_CONTROL_INDICATION
 } iss_primitive_t;
 
-struct iss_data_unit {
+struct iss_data_unit
+{
     unsigned char *destination_addr;
     unsigned char *source_addr;
     uint16_t ethertype;
@@ -23,14 +25,16 @@ struct iss_data_unit {
     void *upper_layer_context;
 };
 
-typedef enum {
-    SECURITY_POLICY_BYPASS,      // Пропуск без защиты
-    SECURITY_POLICY_AUTHENTICATE, // Только аутентификация
-    SECURITY_POLICY_ENCRYPT,      // Шифрование + аутентификация
-    SECURITY_POLICY_DROP          // Блокировка
+typedef enum
+{
+    SECURITY_POLICY_BYPASS,
+    SECURITY_POLICY_AUTHENTICATE,
+    SECURITY_POLICY_ENCRYPT,
+    SECURITY_POLICY_DROP
 } security_policy_t;
 
-struct security_context {
+struct security_context
+{
     security_policy_t policy;
     uint8_t *key;
     size_t key_length;
@@ -38,20 +42,20 @@ struct security_context {
     bool is_secure_iss;
 };
 
-struct iss_instance {
+struct iss_instance
+{
     char interface_name[IFNAMSIZ];
     struct security_context sec_ctx;
     bool is_enabled;
     uint32_t frame_counter;
-    
+
     uint64_t protected_frames_out;
     uint64_t protected_frames_in;
     uint64_t dropped_frames;
-    
+
     int (*original_ma_data_request)(struct iss_data_unit *sdu);
     int (*original_ma_data_indication)(struct iss_data_unit *sdu);
 };
-
 
 int iss_init(const char *interface_name, struct iss_instance **iss);
 void iss_cleanup(struct iss_instance *iss);
@@ -61,8 +65,8 @@ int iss_ma_data_indication(struct iss_instance *iss, struct iss_data_unit *sdu);
 int iss_ma_control_request(struct iss_instance *iss, uint32_t control_code, void *param);
 int iss_ma_control_indication(struct iss_instance *iss, uint32_t indication_code, void *param);
 
-int iss_set_security_policy(struct iss_instance *iss, security_policy_t policy, 
-                           const uint8_t *key, size_t key_len);
+int iss_set_security_policy(struct iss_instance *iss, security_policy_t policy,
+                            const uint8_t *key, size_t key_len);
 int iss_enable_security(struct iss_instance *iss);
 int iss_disable_security(struct iss_instance *iss);
 
@@ -70,6 +74,6 @@ security_policy_t iss_classify_traffic(const struct iss_data_unit *sdu);
 void iss_update_statistics(struct iss_instance *iss, bool is_outgoing, bool is_protected);
 
 int iss_register_driver_callbacks(struct iss_instance *iss,
-                                 int (*data_req)(struct iss_data_unit *),
-                                 int (*data_ind)(struct iss_data_unit *));
+                                  int (*data_req)(struct iss_data_unit *),
+                                  int (*data_ind)(struct iss_data_unit *));
 #endif /* ISS_H */
