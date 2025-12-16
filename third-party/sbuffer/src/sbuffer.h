e8b6a924159b39c1e4f601449aa15d3b85a64a65
dima853 <174457042+dima853@users.noreply.github.com>
2025-12-16 18:23:51 +0300

    ‘something’

--- third-party/sbuffer/src/sbuffer.h
+++ third-party/sbuffer/src/sbuffer.h
@@ -1,6 +1,4 @@
-// sbuffer.h
-#ifndef SBUFFER_H
-#define SBUFFER_H
+#pragma once
 
 #include <stddef.h>
 #include <stdbool.h>
@@ -18,7 +16,6 @@ struct SafeBuffer;
 
 #define SAFE_BUFFER_MAGIC 0x53424652UL
 
-// Создание буферов
 struct SafeBuffer *safe_buffer_create(BufferType type, size_t capacity);
 struct SafeBuffer *safe_buffer_create_string(size_t char_capacity);
 struct SafeBuffer *safe_buffer_create_binary(size_t byte_capacity);
@@ -26,28 +23,21 @@ struct SafeBuffer *safe_buffer_create_int_array(size_t count);
 struct SafeBuffer *safe_buffer_create_float_array(size_t count);
 struct SafeBuffer *safe_buffer_create_struct(size_t element_size, size_t count);
 
-// Валидация
 bool safe_buffer_validate(const struct SafeBuffer *buffer);
 
-// Доступ к данным
 char* safe_buffer_get_string(struct SafeBuffer *buffer);
 uint8_t* safe_buffer_get_binary(struct SafeBuffer *buffer);
 int32_t* safe_buffer_get_int_array(struct SafeBuffer *buffer);
 float* safe_buffer_get_float_array(struct SafeBuffer *buffer);
 void* safe_buffer_get_generic(struct SafeBuffer *buffer);
 
-// Операции
 bool safe_buffer_copy(struct SafeBuffer *dest, const struct SafeBuffer *src);
 bool safe_buffer_append_string(struct SafeBuffer *buffer, const char *str);
 bool safe_buffer_set_string(struct SafeBuffer *buffer, const char *str);
 
-// Информация
 BufferType safe_buffer_get_type(const struct SafeBuffer *buffer);
 size_t safe_buffer_get_capacity(const struct SafeBuffer *buffer);
 size_t safe_buffer_get_length(const struct SafeBuffer *buffer);
 size_t safe_buffer_get_element_size(const struct SafeBuffer *buffer);
 
-// Уничтожение
 void safe_buffer_destroy(struct SafeBuffer *buffer);
-
-#endif // SBUFFER_H
